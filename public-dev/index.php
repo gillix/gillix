f86aa50bc32ea6729256dfdbda2e84d83c8ad925
yury.timoschuk <yury.timoschuk@gmail.com>
2020-03-11 15:25:07 +0200

    path fix

--- public-dev/index.php
+++ public-dev/index.php
@@ -1,5 +1,5 @@
 <?php
- require_once '../../vendor/autoload.php';
+ require_once __DIR__.'/../../vendor/autoload.php';
  
  $launcher = new \glx\HTTP\Launcher(__DIR__.'/../config/launcher-dev.gcml');
  $launcher->run();
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
- require_once '../../vendor/autoload.php';
+ require_once __DIR__.'/../../vendor/autoload.php';
 
  $config = \glx\Config\CachedReader::read(__DIR__.'/../config/launcher.gcml');
  $launcher = new \glx\HTTP\Launcher($config);
